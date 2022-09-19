8da4d41353260885f441ad45e6b52590bab4d759
unacluba <104894720+unacluba@users.noreply.github.com>
2022-09-19 20:41:14 +0200

    Add files via upload

--- llmnr monitor.ps1
+++ llmnr monitor.ps1
@@ -2,11 +2,18 @@ $LLMNR = Resolve-DnsName -LlmnrOnly PleaseSubscribe 2> $null
 if ($LLMNR) {
     $IP = $LLMNR.IpAddress -Join ", "
     $Message = "Hostname: ${env:computername} `nRogue LLMNR Server: $IP"
-    $Body = ConvertTo-Json @{
-        Username = "Bot"
-        Pretext = "Responder Detected!"
-        Text=$Message
-        icon_emoji="ghost"
+    # Slack message settings
+    #$Body = ConvertTo-Json @{
+     #   Username = "Bot"
+     #   Pretext = "Responder Detected!"
+     #   Text=$Message
+     #   icon_emoji="ghost"
+
+# Write to Windows EventLog
+Write-EventLog -LogName Application -Source ResponderPot -EventId 9001 -Message $Message -EntryType Warning
 }
-Invoke-RestMethod https://<slackurl> -Method Post -Body $Body -ContentType 'application/json'
-}
+
+# Write to Slack
+#Invoke-RestMethod https://<slackurl> -Method Post -Body $Body -ContentType 'application/json'
+
+# }
